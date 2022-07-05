843540de5b64f43340e68c4d985702c9fd922b5a
jonasbdc1998 <jonasbdc1998@gmail.com>
2022-07-04 23:21:00 -0300

    feat: add routes to default solution for diet problem

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+
+from src.Diet import Diet
+from flask import Flask, jsonify
+import json
 
 app = Flask(__name__)
 
 
+@app.route("/nutrients")
+def get_nutrients():
+    # Dafault path
+    diet = Diet(nutrients_path = "data/nutrients.json", data_path = "data/data.json")
+    return jsonify(diet._get_data()[0])
+
+@app.route("/data")
+def get_data():
+    # Dafault path
+    diet = Diet(nutrients_path = "data/nutrients.json", data_path = "data/data.json")
+    return jsonify(diet._get_data()[1])
+
+@app.route("/opt")
+def optimization():
+    # Dafault path
+    diet = Diet(nutrients_path = "data/nutrients.json", data_path = "data/data.json")
+    diet._load_constraints()
+    diet._fit()
+    result, amount = diet._result()
+    return jsonify({"Result":result,"amount":amount})
+
 @app.route("/")
 def home():
-    return "<p>Hello World!</p>"
+    return "<h2>App de otimização de compras a partir de nutrientes diários necessários.</h2>"
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
     
--- src/Diet.py
+++ src/Diet.py
@@ -1,12 +1,68 @@
+from ortools.linear_solver import pywraplp
+import json
+
 class Diet:
     """Optimizarion class with generic solution for
     Diet Problem, with google Ortools.
     """    
     def __init__(self,*args,**kwargs):
+        with open(kwargs.get("nutrients_path"),"r") as openfile:
+            self._nutrients = json.load(openfile)
+        with open(kwargs.get("data_path"),"r") as openfile:
+            self._data = json.load(openfile)
+
+    def _get_data(self):
+        return self._nutrients, self._data
         
-        self._foods = args[0]
+    def _load_constraints(self):
+        self._solver = pywraplp.Solver("Diet Price Problem",pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+        self._foods = [self._solver.NumVar(0.0,self._solver.infinity(), item[0]) for item in self._data]
+        # Load Constraints
+        # Define Constraints. One constraint per nutrient
+        self._constraints = []
+        for i, nutrient in enumerate(self._nutrients):
+            # constraint >= nutrient value
+            self._constraints.append(self._solver.Constraint(nutrient[1], self._solver.infinity()))
+            for j, item in enumerate(self._data):
+                nutrient_col = i + 3
+                self._constraints[i].SetCoefficient(self._foods[j],item[nutrient_col])
         
-    def _load(self):
-        pass
+        print('Number of variables:',self._solver.NumVariables())
+        print(f"Number of constraints:{self._solver.NumConstraints()}")
+
+
     def _fit(self):
-        return None
+            # Objective function: Minimize sum of (price nomalized) foods
+        self._objective = self._solver.Objective()
+        for food in self._foods:
+            self._objective.SetCoefficient(food,1)
+        self._objective.SetMinimization()
+
+
+    def _result(self):
+        status = self._solver.Solve()
+        # Check the problem has an optimal solution.
+        if status != self._solver.OPTIMAL:
+            print('The problem does not have an optimal solution!')
+            if status == self._solver.FEASIBLE:
+                print('A potentially suboptimal solution was found.')
+            else:
+                print('The solver could not solve the problem.')
+                return
+        # Display the amounts (in dollars)  to purchase of each food
+        nutrients_result = [0] * len(self._nutrients)
+        result = {}
+        for i, food in enumerate(self._foods):
+            if food.solution_value() > 0.0:
+                # Cost of 30 days
+                result[self._data[i][0]] = 30 * food.solution_value()
+                #print('{}: ${}'.format(self._data[i][0], 365. * food.solution_value()))
+                for j, _ in enumerate(self._nutrients):
+                    nutrients_result[j] += self._data[i][j + 3] * food.solution_value()
+        #print('\nOptimal annual price: ${:.4f}'.format(365. * self._objective.Value()))
+        amount = 30 * self._objective.Value()
+        return result, amount
+
+if __name__ == '__main__':
+    diet = Diet(nutrients_path = "data/nutrients.json", data_path = "data/data.json")
+    print(diet._get_data())
